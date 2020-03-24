a865ad022446f5abc7768a49586552eb0feb94e9
Maximilian <max.vinsum@gmail.com>
2020-03-24 17:32:31 -0400

    Setting up algorithms file

---
+++ src/algorithms/example.js
@@ -0,0 +1,18 @@
+//This is an example of how to export your algorithm
+export function myAlgorithm(matrix) {
+	/*
+		put your code here
+	 */
+
+	//The response of the function should be an array
+	//Each array position should have an object with the de data that you want to draw in the graph
+	let array = [{
+		nodeId: 1,
+		color: "#FFFFFF"
+	}, {
+		nodeId: 2,
+		color: "#FFFFFF"
+	}
+	];
+	return array;
+}
