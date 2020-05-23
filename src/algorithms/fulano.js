8544f339db0e36d2a45a604643e3ad7b16dc0688
Fulano2141 <macuto2142dt@gmail.com>
2020-05-23 16:29:12 -0400

    modified:   src/algorithms/fulano.js

--- src/algorithms/fulano.js
+++ src/algorithms/fulano.js
@@ -1,27 +1,27 @@
 
 export function myfunctionF(matrizAdya) {
 
-    var min = -10;
-    for (var i = 0; i < matrizAdya.length; i++) {
+    let min = -10;
+    for (let i = 0; i < matrizAdya.length; i++) {
         if (min > matrizAdya[0][i]) {
             min = matrizAdya[0][i];
         }
     }
-    var minValue = matrizAdya[0];
-    for (var i = 1; i < matrizAdya[0].length; i++) {
-        var currentValue = matrizAdya[0][i];
+    let minValue = matrizAdya[0];
+    for (let i = 1; i < matrizAdya[0].length; i++) {
+        let currentValue = matrizAdya[0][i];
         if (currentValue < minValue) {
             minValue = currentValue;
         }
     }
-    var maxValue = matrizAdya[0];
-    for (var i = 1; i < matrizAdya[0].length; i++) {
-        var currentValue = matrizAdya[0][i];
+    let maxValue = matrizAdya[0];
+    for (let i = 1; i < matrizAdya[0].length; i++) {
+        let currentValue = matrizAdya[0][i];
         if (currentValue > maxValue) {
             maxValue = currentValue;
         }
     }
-    var data = new Array(1);
+    let data = new Array(1);
     data[0][0] = minValue;
     data[0][1] = maxValue;
     return data;
@@ -29,43 +29,60 @@ export function myfunctionF(matrizAdya) {
 
 
 export function minimizacion(matrizAdya) {
-    var min = [];
+    let min = [];
     // min.push(value);
-    for (var j = 0; j < matrizAdya[0].length; j++) {
-        var minn = matrizAdya[j][0];
-        for (var i = 0; i < matrizAdya.length; i++) {
+    for (let j = 0; j < matrizAdya[0].length; j++) {
+        let minn = matrizAdya[j][0];
+        for (let i = 0; i < matrizAdya.length; i++) {
             if (minn > matrizAdya[i][j])
                 minn = matrizAdya[i][j];
         }
         min.push(minn);
     }
 
-    for (var j = 0; j < matrizAdya[0].length; j++) {
-        for (var i = 0; i < matrizAdya.length; i++) {
+    for (let j = 0; j < matrizAdya[0].length; j++) {
+        for (let i = 0; i < matrizAdya.length; i++) {
             matrizAdya[i][j] = matrizAdya[i][j] - min[j];
         }
     }
-    var min1 = [];
-    for (var i = 0; i < matrizAdya.length; i++) {
-        var minimo = matrizAdya[i][0];
-        for (var j = 0; j < matrizAdya[0].length; j++) {
+    let min1 = [];
+    for (let i = 0; i < matrizAdya.length; i++) {
+        let minimo = matrizAdya[i][0];
+        for (let j = 0; j < matrizAdya[0].length; j++) {
             if (minimo > matrizAdya[i][j])
                 minimo = matrizAdya[i][j];
         }
         min1.push(minimo);
     }
     // console.table(min1);
-    for (var i = 0; i < matrizAdya.length; i++) {
-        for (var j = 0; j < matrizAdya[0].length; j++) {
+    for (let i = 0; i < matrizAdya.length; i++) {
+        for (let j = 0; j < matrizAdya[0].length; j++) {
             matrizAdya[i][j] = matrizAdya[i][j] - min1[i];
         }
     }
     return matrizAdya;
 }
-// var items = [
+// let items = [
 //   [1, 2, 4],
 //   [3, 5, 7],
 //   [9, 2, 6]
 // ];
 
 // console.table(minimizacion(items));
+export function selectionSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
